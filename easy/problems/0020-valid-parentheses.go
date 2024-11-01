fb4ba64f068391b82d8dadc2fc34e5663974d4e9
gcpearse <glen.c.pearse@gmail.com>
2024-11-01 16:57:48 +0000

    Adds solution for isValid

---
+++ easy/problems/0020-valid-parentheses.go
@@ -0,0 +1,27 @@
+package easy
+
+func IsValid(s string) bool {
+	if len(s) < 2 || len(s)%2 != 0 {
+		return false
+	}
+
+	pairs := map[rune]rune{
+		'(': ')',
+		'[': ']',
+		'{': '}',
+	}
+
+	var stack = []rune{}
+
+	for _, char := range s {
+		if _, ok := pairs[char]; ok {
+			stack = append(stack, char)
+		} else if len(stack) > 0 && pairs[stack[len(stack)-1]] == char {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+
+	return len(stack) == 0
+}
