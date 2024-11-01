6a52c03b65f66e7da367fab2bbc4272088af8c48
gcpearse <glen.c.pearse@gmail.com>
2024-11-01 01:26:34 +0000

    Removes line

--- easy/tests/0009-palindrome-number_test.go
+++ easy/tests/0009-palindrome-number_test.go
@@ -20,7 +20,6 @@ var isPalindromeTests = []isPalindromeTest{
 func TestIsPalindrome(t *testing.T) {
 	for _, test := range isPalindromeTests {
 		if got := easy.IsPalindrome(test.x); got != test.want {
-			t.Errorf("x: %v", test.x)
 			t.Errorf("got: %v, wanted: %v", got, test.want)
 		}
 	}
