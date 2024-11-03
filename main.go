500c767685599583707ed83508c3e6a40f23ef54
gcpearse <glen.c.pearse@gmail.com>
2024-11-03 12:20:16 +0000

    Updates main.go

--- main.go
+++ main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 
 	easy "github.com/gcpearse/leetcode-go/easy/problems"
+	hard "github.com/gcpearse/leetcode-go/hard/problems"
 	medium "github.com/gcpearse/leetcode-go/medium/problems"
 )
 
 func main() {
 	easy.Example()
 	medium.Example()
+	hard.Example()
 
 	fmt.Println()
 }
